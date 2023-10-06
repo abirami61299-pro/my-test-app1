df9a64df623d5e54db42ce24ee3b9108c9d005e6
abirami61299-pro <abirami@theproindia.com>
2023-10-06 18:19:30 +0530

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+	ProGamification,
+	updateGameAction,
+} from "@stagetheproindia/react-progamification";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const update = () => {
+		updateGameAction(
+			"e6a1a490-5b8a-440e-bcb6-9707b8b971e1",
+			"651d3a7ff445e71cdc3acb21",
+			"",
+			""
+		);
+	};
+	return (
+		<div className="container">
+			<header className="App">
+				<ProGamification
+					userId={"e6a1a490-5b8a-440e-bcb6-9707b8b971e1"}
+					applicationId={"651d3a7ff445e71f7a3acb16"}
+					clientId={"08ce0068-2906-44d4-b840-b268e5511624"}
+					clientSecret={"2BG8Q~Vsd33.QQ93jcejZkrr5aVyXvxbl_FrubYg"}
+				/>
+			</header>
+			<button onClick={update}>Play</button>
+		</div>
+	);
 }
 
 export default App;
