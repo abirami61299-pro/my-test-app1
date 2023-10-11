6e26e00c2ee0b179e6540064937786db62c822d1
abirami61299-pro <abirami@theproindia.com>
2023-10-11 16:56:05 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,19 @@ import {
 	updateGameAction,
 } from "@stagetheproindia/react-progamification";
 
-function App() {
-	const update = () => {
-		updateGameAction(
-			"e6a1a490-5b8a-440e-bcb6-9707b8b971e1",
-			"651d3a7ff445e71cdc3acb21",
-			"",
-			""
-		);
-	};
+export function App() {
 	return (
 		<div className="container">
 			<header className="App">
 				<div className="game">
 					<ProGamification
-						userId={"e6a1a490-5b8a-440e-bcb6-9707b8b971e1"}
-						applicationId={"651d3a7ff445e71f7a3acb16"}
-						clientId={"08ce0068-2906-44d4-b840-b268e5511624"}
-						clientSecret={"2BG8Q~Vsd33.QQ93jcejZkrr5aVyXvxbl_FrubYg"}
+						userId={"sales@bookwater.com"}
+						applicationId={"64e32cc4293347c3f0446edd"}
+						clientId={"cf72b228-203b-45c7-a2b3-65e141442b03"}
+						clientSecret={"Yba8Q~zXlMHCTdhwLIkQ3T_JvKGrBSWCLisvibwJ"}
 					/>
 				</div>
 			</header>
-			<div className="play">
-				<button className="btn" onClick={update}>
-					Play
-				</button>
-			</div>
 		</div>
 	);
 }
-
-export default App;
